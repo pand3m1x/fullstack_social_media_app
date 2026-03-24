c48945d7f7c7fb4770267df697f414f404ab3e1e
TheRedQueen <pand3m1x@gmail.com>
2026-03-24 15:33:30 -0400

    strcuture for frontend

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+
+function Navbar() {
+
+  return(
+    <nav>
+      <ul>
+        <li><Link to="/login">Login</Link></li>
+        <li><Link to></Link></li>
+      </ul>
+    </nav>
+  )
+}
