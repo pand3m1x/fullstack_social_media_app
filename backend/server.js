21a25c03e6078b7cc02e644e408bc693af589408
TheRedQueen <pand3m1x@gmail.com>
2026-03-24 15:58:17 -0400

    added notes about cors

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import cors from 'cors'
 const port = process.env.PORT || 3000
 
 app.use(express.json())
-app.use(cors())
+app.use(cors()) //['http://localhost:5173'] < This is for anything that communicates with the backend, telling it, 'it's okay' 
 
 app.use('/api/posts', postRoutes)
 app.use('/api/users', userRoutes)
