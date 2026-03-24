4fee291ef9c4e28b4bedd42f4f0970e66581522a
TheRedQueen <pand3m1x@gmail.com>
2026-03-24 10:10:45 -0400

    added time stamp to post

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -11,6 +11,10 @@ const postSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  createdAt:{
+    type: Date,
+    default: Date.now()
+  },
   author: {
     type: mongoose.Types.ObjectId,
     ref: 'User',
