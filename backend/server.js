ee75eca8e9d42dea2908fe95ab4ca3f6c731489c
TheRedQueen <pand3m1x@gmail.com>
2026-03-24 09:41:51 -0400

    No cute emojis this time, follow along, setted up structure for frontend and backend

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.get('/', (req,res) => {
+  res.send('hello world!')
+})
+
+app.listen(port,() => console.log(`listening to port: http://localhost:${port}`))
