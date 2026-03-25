9b4c81c08ee4f376a272e73d2fbb17fafee32fee
TheRedQueen <pand3m1x@gmail.com>
2026-03-25 09:59:13 -0400

    added register form, but something broke

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,21 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import './App.css';
 import { Routes, Route } from "react-router-dom";
 
-import Home from "./components/Home";
-import Login from "./components/Login";
-import Register from "./components/Register";
+// import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import Navbar from "./components/Navbar";
-// import Navbar from "./components/Navbar";
 
 function App(){
-  useEffect(() => {
-    // async function getData() {
-    //   const response = await fetch('http://localhost:3000/api/posts')
-    //   const data = await response.json()
-    //   console.log(data)
-    // }
-    // getData()
-  }, [])
+  // useEffect(() => {
+  //   // async function getData() {
+  //   //   const response = await fetch('http://localhost:3000/api/posts')
+  //   //   const data = await response.json()
+  //   //   console.log(data)
+  //   // }
+  //   // getData()
+  // }, [])
 
   return(
     <>
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,33 @@
+import { useState } from "react";
+
 function Register() {
+
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: ''
+  })
+
+  const handleChange = (e) =>{
+      setFormData({
+        ...formData,
+        [e.target.name]: e.target.value // [] makes it dynamic instead of listing each input individually
+      })
+  }
 
   return(
     <div>
       <h1>Register</h1>
+      <form>
+        <label htmlFor="username">Username</label>
+        <input value={formData.username} onChange={handleChange} type="text" id="username" name="username" required/>
+        <label htmlFor="email">Email</label>
+        <input value={formData.email} onChange={handleChange} type="email" id="email" name="email" required />
+        <label htmlFor="password">Password</label>
+        <input value={formData.password} onChange={handleChange} type="password" id="password" name="password" required />
+        <button type="submit">Register</button>
+
+      </form>
     </div>
   )
 
