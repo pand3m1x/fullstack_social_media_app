c5653286581feafc393ff9e10fc82d5287990aa5
TheRedQueen <pand3m1x@gmail.com>
2026-03-25 13:38:31 -0400

    copied Register and moved it to login and updated login with relevant information

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,73 @@
+import { useState } from "react";
+import { userClient } from "../clients/api"
+import { useUser } from "../context/UserContext"
+import { useNavigate } from "react-router-dom";
+
 function Login() {
+
+  const { setUser } = useUser()
+
+  const navigate = useNavigate()
+  // const value = useUser()
+  // console.log (value)
+
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  })
+
+  const handleChange = (e) =>{
+      setFormData({
+        ...formData,
+        [e.target.name]: e.target.value // [] makes it dynamic instead of listing each input individually
+      })
+  }
+
+  const handleSubmit = async (e) => {
+
+    e.preventDefault()
+    console.log(formData)
+
+    // send the form data to our backend
+    try {
+
+      const { data } = await userClient.post('/login', formData) // response.data also works: where the Response has other properties (like the HTTP code, error information etc)
+      console.log(data)
+
+      // take the token and store it locally
+  
+        localStorage.setItem("token", data.token) //or response.data.token
+        
+      // save some user data in our state
+  
+      setUser(data.user)
+        
+      // take the user to a different page
+
+      navigate("/feed")
+
+    } catch (err) {
+
+        console.log(err)
+        alert(err.message)
+
+    }
+  }
 
   return(
     <div>
       <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+
+        <label htmlFor="email">Email</label>
+        <input value={formData.email} onChange={handleChange} type="email" id="email" name="email" required />
+        
+        <br/><label htmlFor="password">Password</label>
+        <input value={formData.password} onChange={handleChange} type="password" id="password" name="password" required />
+        
+        <br/><button type="submit">Login</button>
+
+      </form>
     </div>
   )
 
