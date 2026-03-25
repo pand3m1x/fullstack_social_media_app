f856951b34285ac1c6eeacc7f09837ff8e6ea545
TheRedQueen <pand3m1x@gmail.com>
2026-03-25 10:08:06 -0400

    Something is still broken, added form information

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,20 @@
-// import { useEffect } from "react";
-import './App.css';
-import { Routes, Route } from "react-router-dom";
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
 
-// import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Navbar from "./components/Navbar";
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Feed from './pages/Feed'
 
+import Navbar from './components/Navbar'
 function App(){
-  // useEffect(() => {
-  //   // async function getData() {
-  //   //   const response = await fetch('http://localhost:3000/api/posts')
-  //   //   const data = await response.json()
-  //   //   console.log(data)
-  //   // }
-  //   // getData()
-  // }, [])
 
   return(
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/feed" element={<Feed />} />
       </Routes>
     </>
   )
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -15,16 +15,33 @@ function Register() {
       })
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(formData)
+
+    // send the form data to our backend
+
+    // take the token and store it locally
+
+    // save some user data in our state
+
+    // take the user to a different page
+  }
+
   return(
     <div>
       <h1>Register</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
+
         <label htmlFor="username">Username</label>
         <input value={formData.username} onChange={handleChange} type="text" id="username" name="username" required/>
+
         <label htmlFor="email">Email</label>
         <input value={formData.email} onChange={handleChange} type="email" id="email" name="email" required />
+        
         <label htmlFor="password">Password</label>
         <input value={formData.password} onChange={handleChange} type="password" id="password" name="password" required />
+        
         <button type="submit">Register</button>
 
       </form>
